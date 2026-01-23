abc5575ef889062efbfb5e08f5613532899bfdcc
giorgigelashvili12 <123294128+giorgigelashvili12@users.noreply.github.com>
2026-01-23 18:05:06 +0400

    Updated Functioniality

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,2 +1,2 @@
 export const API_BASE = "https://soteria-q27e.onrender.com/api/v1";
-export const CHECKOUT_WEB = "https://soteria-client.onrender.com";
+export const CHECKOUT_WEB = "https://soteria-client.onrender.com/checkout/:clientSecret";
